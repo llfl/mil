8374446d706092a1e299410e65bcb08d7dcab018
Kun <quinnHugh@hotmail.com>
2019-09-04 00:06:06 +0800

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -34,6 +34,8 @@ class DataGenerator(object):
         self.scale, self.bias = None, None
 
         demo_file = FLAGS.demo_file
+        if demo_file == None :
+            demo_file = 'None'
         demo_file = natsorted(glob.glob(demo_file + '/*pkl'))
         self.dataset_size = len(demo_file)
         if FLAGS.train and FLAGS.training_set_size != -1:
